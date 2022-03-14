78bd9f00eb4349b6ad5db55bd4376acca8f95db5
rcosserver <61562914+rcosserver@users.noreply.github.com>
2022-03-14 16:08:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,7 +64,7 @@ def message(message):
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     if call.data == '1':
-        update.message.reply_text(text=f"Вы хотите сгенерировать план на сегодня и отчёт за вчера?", reply_markup=ReplyKeyboardRemove())
+        bot.message.reply_text(text=f"Вы хотите сгенерировать план на сегодня и отчёт за вчера?", reply_markup=ReplyKeyboardRemove())
         bot.send_message(call.message.chat.id, 'Продолжаем разговор')
 
         
